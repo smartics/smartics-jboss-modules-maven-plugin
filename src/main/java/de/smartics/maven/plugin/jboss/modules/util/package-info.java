3534aa54ae73c02f92eaa71a1e533476094d18ff
Anton Kronseder <anton.kronseder@smartics.de>
2015-04-23 13:39:37 +0200

    Changed license date to 2015.

--- src/main/java/de/smartics/maven/plugin/jboss/modules/util/package-info.java
+++ src/main/java/de/smartics/maven/plugin/jboss/modules/util/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2014 smartics, Kronseder & Reiner GmbH
+ * Copyright 2013-2015 smartics, Kronseder & Reiner GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * Utilities, sometimes duplicated code from other projects to keep the
  * number of dependencies small.
